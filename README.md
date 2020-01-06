b4125c4d8787e7b1f9a3f2393ea39aabed3f7b2a
Kurt LaVacque <lavacquek@icloud.com>
2020-01-06 15:00:54 -0600

    Create Proposal

--- README.md
+++ README.md
@@ -1,14 +1,51 @@
-# Vibo
+# Date Night Recipes
 
 ## Description
 
+When you ask a friend to come over for dinner and drinks
+And you want to impress them with your cooking and cocktail skills
+You can use this web app to up your game and show them a luxurious meal with fancy drink.
+
 ## User Stories
 
-### 2 - 3 stories
+As a  Undecisive Date Planner
+I want to Find good recipes 
+So that I can cook for date night
+
+As a Undecisive Date Planner
+I want to Find good cocktail recipes  
+So that I can prepare them for date night
 
 ## Wireframe
 
+
+
+## User Flow
+
+The user opens up webpage
+#### Page 1 Meal Search
+They are asked to search for a specific meal, main ingredient to a meal, or meal category
+
+#### Page 2 Cocktail Search
+They are asked to search for a specific cocktail, type of alcohol in cocktail, specific ingredient in cocktail
+
+#### Page 3 
+The user will now be shown a meal recipe and a cocktail recipe based on the user search
+
 ## APIS To Be Used
 
+The CocktailDB API
+https://www.thecocktaildb.com/api.php
+
+The Meal API
+https://www.themealdb.com/api.php
+
 ## Rough Breakdown
+
+
+
+
+
+
+
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,75 +1,10 @@
-var client_ID = "6faa4506df6e4d3ab6c894bfa461fdb4";
+https://openwhyd.org/adrien/playlist/61/format=links
 
-console.log(client_ID)
-// // var client_Secret = "43e0f49befb44083bb8549c5b5c204ac";
+var queryURL = "https://openwhyd.org/adrien/playlist/10?format=links&limit=1" // + "q=" + search_city + "&units=imperial&appid=" + APIKey;
 
-// // var search = "Reggee";
-// // var type = "playlist";
-// // var limit = "10";
-
-// // var queryURL = "https://api.spotify.com/v1/search?" + "q=" + search + "&type=" + type + "&limit=" + limit + -"H "Authorization: Bearer {your access token};
-
-// // console.log(queryURL)
-    
-// // // Do an AJAX Request
-// // $.ajax({url: queryURL,method: "GET"}).then(function(response) {
-// //     // Assign Variables to Request
-    
-// //     console.log(response)
+$.ajax({url: queryURL, method: "GET"}).then(function(response) {
+    // Assign Variables to Request
+    console.log(response)
     
     
-// // })  
-
-
-// // $.ajax({
-// //     method: "GET",
-// //     url: `https://api.spotify.com/v1/search?q=track:antarctica&type=track/api/token`
-// //   }).then(function(data) {
-// //     console.log(data);
-// //   });
-
-// //   function getTopTracks(access_token) {
-// //     $.ajax({
-// //       url: 'https://api.spotify.com/v1/me/top/tracks?limit=10',
-// //       headers: {
-// //         'Authorization': 'Bearer ' + access_token
-// //       },
-// //       success: function(response) {
-// //         $(".recommendations").show();
-// //         mapOverSongs(response.items);
-// //       }
-// //     });
-// //   }
-
-
-// // Get the hash of the url
-// const hash = window.location.hash
-// .substring(1)
-// .split('&')
-// .reduce(function (initial, item) {
-//   if (item) {
-//     var parts = item.split('=');
-//     initial[parts[0]] = decodeURIComponent(parts[1]);
-//   }
-//   return initial;
-// }, {});
-// window.location.hash = '';
-
-// // Set token
-// let _token = hash.access_token;
-
-// const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-// // Replace with your app's client ID, redirect URI and desired scopes
-// const clientId = '6faa4506df6e4d3ab6c894bfa461fdb4';
-// const redirectUri = 'http://localhost:8888';
-// const scopes = [
-//   'user-read-birthdate',
-//   'user-read-email',
-//   'user-read-private'
-// ];
-
-// // If there is no token, redirect to Spotify authorization
-// if (!_token) {
-//   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
-// }
+})  
