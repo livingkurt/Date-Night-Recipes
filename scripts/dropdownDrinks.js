45ba10c945b4c7f82b7479af60b80af3c940cda4
Patrick Duquette <duquetteman@gmail.com>
2020-01-08 14:05:42 -0600

    Updating navbar functionality.

--- scripts/dropdownDrinks.js
+++ scripts/dropdownDrinks.js
@@ -1,5 +1,9 @@
 var search_i_d_e = $("#search_i_d");
 
+// function drinkSelection() {
+//     document.getElementsByClassName("drinkNav")
+
+// onclick(drinkSelection)
 
 console.log(window.location.search);
 var searchTerm = window.location.search.slice(7);
